c49b8ff3151700abff0efa7fcac9de1e978c8002
alexantaki16 <alexander.antaki1@marist.edu>
2017-10-12 20:41:13 -0400

    Update JudgingMoose.java
    Added comments

--- Java/JudgingMoose.java
+++ Java/JudgingMoose.java
@@ -1,11 +1,19 @@
+/*
+*  Author: Alex Antaki
+*  Date: October 12, 2017
+*  This program will calculate which type of moose you see based off of the antler count and
+*  how many are on each side.
+*/
 import java.util.Scanner;
 public class JudgingMoose{
   public static void main(String[] args){
     Scanner input = new Scanner(System.in);
 
+    //Have the user input the tines on the left and right sides
     int tinesOnLeft = input.nextInt();
     int tinesOnRight = input.nextInt();
 
+    //Use lengthy if statement to check which moose we have
     if(tinesOnLeft == 0 && tinesOnRight == 0)
      System.out.println("Not a moose");
     else if(tinesOnLeft > tinesOnRight){
