03fc52fb5d5254ed5f6165e14ec8d0e90432a5e0
alexantaki16 <alexander.antaki1@marist.edu>
2017-10-12 20:44:18 -0400

    Update Kornislav.java
    Added comments

--- Java/Kornislav.java
+++ Java/Kornislav.java
@@ -1,14 +1,21 @@
+/*
+*  Author: Alex Antaki
+*  Date: October 12, 2017
+*  This program will is the solution to the Kornislav the turtle problem on Kattis.com
+*/
 import java.util.*;
 public class Kornislav {
   public static void main(String[] args){
     Scanner input = new Scanner(System.in);
 
-    //Create an array to store the input and use a for loop to populate it
+    //Create an array to store the input
     int[] directions = new int[4];
+    
+    //Use a for loop to populate the array
     for(int i = 0; i < directions.length; i++){
       directions[i] = input.nextInt();
     }
-    //Sort the array to easily locate the smalles and second to lagerst values
+    //Sort the array to easily locate the smallest and penultimate values
     Arrays.sort(directions);
 
     //Store the rectangle and display it
